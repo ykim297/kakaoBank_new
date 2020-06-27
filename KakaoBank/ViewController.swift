9df211e2030ccdc633d27cd01a33df05602cdd15
Young Kim <ykim297@gmail.com>
2020-06-27 17:26:05 +0900

    Initial Commit

---
+++ KakaoBank/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KakaoBank
+//
+//  Created by Yong Seok Kim on 2020/06/27.
+//  Copyright © 2020 Yong Seok Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
